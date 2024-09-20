5f4061bb066cb3dc95daaa1517396ec41dd7ef7c
Raghav Agrawal <raghav170101@gmail.com>
2024-09-20 18:23:07 +1000

    login signup apis

--- routes/user.controllers.js
+++ routes/user.controllers.js
@@ -5,12 +5,9 @@ const Role = require("../helpers/role");
 const jwt = require("../helpers/jwt");
 const sendEmail = require("../helpers/email");
 //routes
-router.patch("/resetPassword/:token", resetPassword);
 router.post("/login", login);
 router.post("/register", register);
-router.get("/", jwt(Role.Admin), getAll);
 router.get("/current", jwt(), getCurrent);
-router.post("/forgotPassword", forgotPassword);
 router.get("/:id", getById);
 
 module.exports = router;
@@ -42,17 +39,6 @@ function login(req, res, next) {
     .catch((error) => next(error));
 }
 
-function getAll(req, res, next) {
-  const currentUser = req.user;
-  if (currentUser.role !== Role.Admin) {
-    return res.status(401).json({ message: "Not Authorized!" });
-  }
-  userServices
-    .getAll()
-    .then((users) => res.json(users))
-    .catch((err) => next(err));
-}
-
 function getCurrent(req, res, next) {
   console.log(req);
   userServices
@@ -71,39 +57,5 @@ function getById(req, res, next) {
       }
       return res.json(user);
     })
-    .catch((error) => next(error));
-}
-
-async function forgotPassword(req, res, next) {
-  await userServices
-    .forgotPassword(req.body)
-    .then(async (resetToken) => {
-      // console.log("ok", resetToken);
-      const resetURL = `https://itis-group35.vercel.app/resetPassword/${resetToken}`;
-      const message = resetURL;
-
-      await sendEmail({
-        email: req.body.email,
-        subject: "Password change request received",
-        message: message,
-      });
-
-      // Respond with success message
-      return res.status(200).json({
-        status: "success",
-        message,
-      });
-    })
-    .catch((error) => next(error));
-}
-
-async function resetPassword(req, res, next) {
-  userServices
-    .resetPassword(req)
-    .then(() =>
-      res.json({
-        message: `PC Success`,
-      })
-    )
     .catch((error) => next(error));
 }
--- services/user.services.js
+++ services/user.services.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
-const config = require("../config.json");
 const db = require("../helpers/db");
 const User = db.User;
 
@@ -19,9 +19,13 @@ async function login({ email, password }) {
   const user = await User.findOne({ email });
   console.log("user model", user);
   if (user && bcrypt.compareSync(password, user.password)) {
-    const token = jwt.sign({ sub: user.id, role: user.role }, config.secret, {
-      expiresIn: "7d",
-    });
+    const token = jwt.sign(
+      { sub: user.id, role: user.role },
+      process.env.secret,
+      {
+        expiresIn: "7d",
+      }
+    );
     return { ...user.toJSON(), token };
   }
 }
@@ -35,48 +39,9 @@ async function getById(id) {
   return await User.findById(id);
 }
 
-async function forgotPassword({ email }) {
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw `Could not find user: ${email}`;
-  }
-  const resetToken = crypto.randomBytes(32).toString("hex");
-  user.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-  user.passwordResetTokenExpiration = Date.now() + 10 * 60 * 1000;
-  await user.save({ validateBeforeSave: false });
-  return resetToken;
-}
-
-async function resetPassword(req) {
-  const token = crypto
-    .createHash("sha256")
-    .update(req.params.token)
-    .digest("hex");
-  console.log(token);
-  const user = await User.findOne({
-    passwordResetToken: token,
-  });
-
-  if (!user) {
-    throw `Invalid Token`;
-  }
-
-  user.password = bcrypt.hashSync(req.body.password, 10);
-  user.passwordResetToken = undefined;
-  user.passwordResetTokenExpiration = undefined;
-  user.passwordChangedAt = Date.now();
-
-  user.save();
-}
-
 module.exports = {
   login,
   getAll,
   getById,
   register,
-  forgotPassword,
-  resetPassword,
 };
